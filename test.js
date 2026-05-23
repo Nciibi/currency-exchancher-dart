eafab8af001a46e7c5aa39a9e5446712f7895960
Nciibi <ncibia77@gmail.com>
2026-05-23 23:21:16 +0100

    555

--- test.js
+++ test.js
@@ -4,7 +4,6 @@ const serviceAccount = require("./serviceAccountKey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 }); 
-        
 const db = admin.firestore();
 const app = express();
 app.use(express.json());
